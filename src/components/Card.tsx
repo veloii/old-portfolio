8a76736122fc2f4e4765e6e60b2dd3532a19837b
zelrdev <zelr.apps@gmail.com>
2023-01-21 22:10:17 +0000

    feat: add animations for tech cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,32 +15,30 @@ export const Card = ({
   desc: string;
 }) => {
   return (
-    <div className="w-full p-4 border-y">
-      <motion.div
-        initial={{
-          background: `radial-gradient(circle at 100% 0px, ${color}50, rgb(249, 250, 251) 70px)`,
-        }}
-        whileHover={{
-          background: `radial-gradient(circle at 100% 0px, ${color}50, rgb(249, 250, 251) 170px)`,
-        }}
-        transition={{
-          duration: 0.5,
-          ease: "easeInOut",
-        }}
-        className="w-full relative shadow-sm bg-gray-50 rounded-2xl border"
+    <motion.div
+      initial={{
+        background: `radial-gradient(circle at 100% 0px, ${color}50, rgb(249, 250, 251) 70px)`,
+      }}
+      whileHover={{
+        background: `radial-gradient(circle at 100% 0px, ${color}50, rgb(249, 250, 251) 170px)`,
+      }}
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
+      }}
+      className="w-full relative shadow-sm bg-gray-50 rounded-2xl border"
+    >
+      <div className="absolute -top-[17px] shadow-md rounded-md -right-[17px]">
+        <img className="h-8 rounded-md border" src={`/${image}.webp`} />
+      </div>
+      <p
+        className={`text-center pointer-events-none ${className} text-2xl font-black pt-4`}
       >
-        <div className="absolute -top-[17px] shadow-md rounded-md -right-[17px]">
-          <img className="h-8 rounded-md border" src={`/${image}.webp`} />
-        </div>
-        <p
-          className={`text-center pointer-events-none ${className} text-2xl font-black pt-4`}
-        >
-          {title}
-        </p>
-        <p className="text-center text-gray-500 pointer-events-none font-semibold text-xs p-3 pt-1">
-          {desc}
-        </p>
-      </motion.div>
-    </div>
+        {title}
+      </p>
+      <p className="text-center text-gray-500 pointer-events-none font-semibold text-xs p-3 pt-1">
+        {desc}
+      </p>
+    </motion.div>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,21 @@ import { Lines } from "./Lines";
 import { motion } from "framer-motion";
 import { Card } from "./Card";
 
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
 export const Hero = () => {
   return (
     <div className="max-w-7xl flex relative min-h-screen mx-auto">
@@ -30,53 +45,68 @@ export const Hero = () => {
 
           <Text />
         </div>
-        <div className="w-full flex pt-4">
-          <Card
-            color={"#01b7d6"}
-            className={"text-[#01b7d6]"}
-            title={"TailwindCSS"}
-            desc={
-              "I used Tailwind to create beautiful UIs. I've used other frameworks like MUI, but they are too opinionated for me."
-            }
-            image="tw"
-          />
-          <Card
-            image="ts"
-            color={"#017acc"}
-            className={"text-[#017acc]"}
-            title={"Typescript"}
-            desc={
-              "I have 2 years of experience in Typescript and use it in all my projects. It's my goto language because of it's strong types."
-            }
-          />
-          <Card
-            image="next"
-            color={"#000000"}
-            className={"text-[#000000]"}
-            title={"Next.js"}
-            desc={
-              "I've been working with next.js since I started React and I use it in many of my projects. I'm very proficient in it."
-            }
-          />
-          <Card
-            image="react"
-            color={"#61dafb"}
-            className={"text-[#1694b7]"}
-            title={"React.js"}
-            desc={
-              "React.js is a gold standard for web applications in 2023. With 2 years of experience of React.js I believe I am experienced in it."
-            }
-          />
-          <Card
-            image="prisma"
-            color={"#0c344b"}
-            className={"text-[#0c344b]"}
-            title={"Prisma"}
-            desc={
-              "Prisma is my goto ORM for the backend of an application. It provides an easy and good way to manage my databases."
-            }
-          />
-        </div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="w-full flex pt-4"
+        >
+          <motion.div variants={item} className="w-full p-4 border-y">
+            <Card
+              color={"#01b7d6"}
+              className={"text-[#01b7d6]"}
+              title={"TailwindCSS"}
+              desc={
+                "I used Tailwind to create beautiful UIs. I've used other frameworks like MUI, but they are too opinionated for me."
+              }
+              image="tw"
+            />
+          </motion.div>
+          <motion.div variants={item} className="w-full p-4 border-y">
+            <Card
+              image="ts"
+              color={"#017acc"}
+              className={"text-[#017acc]"}
+              title={"Typescript"}
+              desc={
+                "I have 2 years of experience in Typescript and use it in all my projects. It's my goto language because of it's strong types."
+              }
+            />
+          </motion.div>
+          <motion.div variants={item} className="w-full p-4 border-y">
+            <Card
+              image="next"
+              color={"#000000"}
+              className={"text-[#000000]"}
+              title={"Next.js"}
+              desc={
+                "I've been working with next.js since I started React and I use it in many of my projects. I'm very proficient in it."
+              }
+            />
+          </motion.div>
+          <motion.div variants={item} className="w-full p-4 border-y">
+            <Card
+              image="react"
+              color={"#61dafb"}
+              className={"text-[#1694b7]"}
+              title={"React.js"}
+              desc={
+                "React.js is a gold standard for web applications in 2023. With 2 years of experience of React.js I believe I am experienced in it."
+              }
+            />
+          </motion.div>
+          <motion.div variants={item} className="w-full p-4 border-y">
+            <Card
+              image="prisma"
+              color={"#0c344b"}
+              className={"text-[#0c344b]"}
+              title={"Prisma"}
+              desc={
+                "Prisma is my goto ORM for the backend of an application. It provides an easy and good way to manage my databases."
+              }
+            />
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
